00bc7c67bc0d4adc4a9bd582e28c194e09c1ff2c
sahana <jayadevaprakash.s@northeastern.edu>
2025-10-20 23:35:20 -0400

    rev

--- src/config/db.js
+++ src/config/db.js
@@ -10,20 +10,16 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
     logging: false,
-       // Make SSL optional and configurable for local development
-    dialectOptions: (() => {
-      const useSsl = process.env.DB_SSL === "true";
-      if (process.env.NODE_ENV === "test") return {};
-      if (useSsl) {
-        return {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        };
-      }
-      return {};
-    })(),
+    // Make SSL optional and configurable for local development
+    dialectOptions:
+      process.env.NODE_ENV != "test"
+        ? {
+            ssl: {
+              require: true, // forces SSL connection
+              rejectUnauthorized: false, // use 'false' if you do not have a CA certificate; not ideal for production
+            },
+          }
+        : {},
   }
 );
 
